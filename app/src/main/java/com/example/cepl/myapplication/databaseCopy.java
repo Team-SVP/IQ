604fb06051e31a917cbae9426cb5ca160cca98c2
Vitthal Padwal <vitthalpadwal89@gmail.com>
2017-04-04 20:36:24 +0530

    Database file copy from assets to application device code working and added bacground image and app icon

--- app/src/main/java/com/example/cepl/myapplication/databaseCopy.java
+++ app/src/main/java/com/example/cepl/myapplication/databaseCopy.java
@@ -1,11 +1,14 @@
 package com.example.cepl.myapplication;
 
 import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.res.AssetManager;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -16,13 +19,20 @@ import java.io.OutputStream;
  */
 
 public class databaseCopy  {
-
-    private static String DB_PATH = "/data/data/com.example.cepl.myapplication/databases/";
+    //data/user/0/com.example.cepl.myapplication/files/databases/svp.db
+    private static String DB_PATH;
+    //private static String DB_PATH = "/data/data/com.example.cepl.myapplication/databases/";
     private static String DB_NAME = "svp.db";
     private SQLiteDatabase myDataBase;
 
     public databaseCopy (Context context) {
-
+        ContextWrapper cw = new ContextWrapper( context.getApplicationContext());
+        //databaseCopy.DB_PATH = cw.getFilesDir().getAbsolutePath()+ "/databases/"; //edited to databases
+        if(android.os.Build.VERSION.SDK_INT >= 17) {
+            databaseCopy.DB_PATH = context.getApplicationInfo().dataDir + "/databases/";
+        } else {
+            databaseCopy.DB_PATH = "/data/data/" + context.getPackageName() + "/databases/";
+        }
         boolean dbExist = checkDataBase();
 
         if (dbExist) {
@@ -56,7 +66,7 @@ public class databaseCopy  {
             myInput = myContext.getAssets().open("svp.db");
             // transfer bytes from the inputfile to the
             // outputfile
-            myOutput = new FileOutputStream(DB_PATH + "svp.db");
+            myOutput = new FileOutputStream(databaseCopy.DB_PATH + "svp.db");
             while ((length = myInput.read(buffer)) > 0) {
                 myOutput.write(buffer, 0, length);
             }
@@ -75,21 +85,28 @@ public class databaseCopy  {
 
     private boolean checkDataBase(){
 
-        SQLiteDatabase checkDB = null;
-
+        //SQLiteDatabase checkDB = null;
+        boolean checkDB = false;
         try{
-            String myPath = DB_PATH + DB_NAME;
-            checkDB = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
-
-        }catch(SQLiteException e){
+            String myPath = databaseCopy.DB_PATH + DB_NAME;
+            //checkDB = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
+            File f = new File(myPath);
+            if(f.exists()) {
+                checkDB = true;
+            } else {
+                checkDB = false;
+            }
+        }catch(Exception e){
 
             //database does't exist yet.
         }
+        /*
         if(checkDB != null){
 
             checkDB.close();
 
-        }
-        return checkDB != null ? true : false;
+        } */
+        //return checkDB != null ? true : false;
+        return (checkDB);
     }
 }
